e2029bf959f8c6a0580f10fc1d3b2cc5a0646ce8
elif-ozkan <94003321+elif-ozkan@users.noreply.github.com>
2023-02-06 21:53:12 +0300

    Add files via upload

---
+++ İlişkili Veri Tabanı Sorgulama Uygulaması/İlişkili Veri Tabanı Sorgulama Uygulaması/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace İlişkili_Veri_Tabanı_Sorgulama_Uygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.BtnGetir = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 239);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(762, 189);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(734, 142);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // BtnGetir
+            // 
+            this.BtnGetir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnGetir.Location = new System.Drawing.Point(147, 174);
+            this.BtnGetir.Name = "BtnGetir";
+            this.BtnGetir.Size = new System.Drawing.Size(199, 45);
+            this.BtnGetir.TabIndex = 2;
+            this.BtnGetir.Text = "Sonucu Getir";
+            this.BtnGetir.UseVisualStyleBackColor = true;
+            this.BtnGetir.Click += new System.EventHandler(this.BtnGetir_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(449, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Sql Hakkında";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnGetir);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button BtnGetir;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ İlişkili Veri Tabanı Sorgulama Uygulaması/İlişkili Veri Tabanı Sorgulama Uygulaması/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace İlişkili_Veri_Tabanı_Sorgulama_Uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //Data Source =LAPTOP-9MADQ7Q9\SQLEXPRESS01
+        //LAPTOP-9MADQ7Q9\SQLEXPRESS01
+        //Bağlantı adresi=Data Source=LAPTOP-9MADQ7Q9\SQLEXPRESS01;Initial Catalog="Film Arşivim";Integrated Security=True
+        SqlConnection baglantı = new SqlConnection("Data Source=LAPTOP-9MADQ7Q9\\SQLEXPRESS01;Initial Catalog=Sorgular;Integrated Security=True");
+        string sorgu;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sorgu = richTextBox1.Text;
+            SqlConnection baglantı = new SqlConnection("Data Source=LAPTOP-9MADQ7Q9\\SQLEXPRESS01;Initial Catalog=Sorgular;Integrated Security=True");
+
+            baglantı.Open();
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter("Select*from TBLÜRÜNLER", baglantı);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch {
+                MessageBox.Show("Sorgunuzu kontrol edin!");
+            }
+            baglantı.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+
+        private void BtnGetir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                baglantı.Open();
+                sorgu = richTextBox1.Text;
+                SqlCommand komut = new SqlCommand(sorgu, baglantı);
+                komut.ExecuteNonQuery();
+
+                SqlDataAdapter d = new SqlDataAdapter("select*from TBLFILMLER", baglantı);
+                DataTable dt = new DataTable();
+                d.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+            }
+            catch
+            {
+                MessageBox.Show("Sorgunuzu kontrol edin", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Sql ekleme,silme ve güncellemeye dayanan veri tabanları için kullanılan" +
+                "bir sorgulama dilidir";
+        }
+    }
+}
